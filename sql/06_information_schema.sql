7fb732d551a96a0054cb8360c24e509307bfe265
ross-bi <ross7heaven@gmail.com>
2026-03-24 09:52:16 +0800

    revise direction

--- sql/06_information_schema.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-  table_name,
-  ordinal_position,
-  column_name,
-  data_type,
-  is_nullable
-FROM `ross-bi-project-03.traffic_ad_roi_clean.INFORMATION_SCHEMA.COLUMNS`
-ORDER BY table_name, ordinal_position;
